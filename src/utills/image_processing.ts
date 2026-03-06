0261f801965d8a3be047f5bc8f02008cb4952cb2
Imad <mymail.imad@gmail.com>
2026-03-06 16:53:52 +0200

    add return value for resizeImage function

--- src/utills/image_processing.ts
+++ src/utills/image_processing.ts
@@ -1,11 +1,12 @@
 import sharp from 'sharp';
 
+// Resizes an image to the specified width and height and saves it to the output path.
 const resizeImage = async (
     originalPath: string,
     outputPath: string,
     width: number,
     height: number,
-) => {
+): Promise<void> => {
     await sharp(originalPath).resize(width, height).toFile(outputPath);
 };
 
